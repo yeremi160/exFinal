7fc31411e68663893ee41b7c12f43656e9d46599
Yeremi <yeremigonzalezyanes@gmail.com>
2020-06-05 23:58:24 -0600

    se le dio tamaño a la ventana TK

---
+++ examen.py
@@ -0,0 +1,12 @@
+from tkinter import ttk
+from tkinter import *
+import math
+import datetime
+class Desk:
+    def __init__(self, window):
+#ventana
+        ancho = 450
+        alto = 300
+        
+     
+        self.wind = window
